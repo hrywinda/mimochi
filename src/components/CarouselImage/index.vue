4cddd2c790b223eef6f1036d7bb8c4da8e851550
Windahr <wharyani@juraganmaterial.id>
2023-08-23 14:24:59 +0700

    feat: add carousel component

---
+++ src/components/CarouselImage/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="carousel" :style="[`background-image: url(${url})`]">
+    <agile
+    :initialSlide="0"
+    :autoplay="autoplay"
+    :navButtons="false"
+    :dots="true"
+    :speed="300"
+    @after-change="showCurrentSlide($event)">
+      <div v-for="(slide, index) in slides" :key="index">
+        <div class="carousel-caption">
+          <div class="text-white title">{{slide.title}}</div>
+          <div class="text-white bold heading-2">{{slide.subtitle}}</div></div>
+        </div>
+    </agile>
+  </div>
+</template>
+
+<script>
+
+import { VueAgile } from 'vue-agile'
+
+export default {
+  name: 'CarouselImage',
+  components: {
+  'agile': VueAgile
+  },
+  props: {
+    slides: {
+      type: Array,
+      required: false,
+    },
+    autoplay:{
+      type: Boolean,
+      required: false,
+      default: true
+    },
+  },
+  data() {
+    return {
+     url : ''
+    }
+  },
+  mounted(){
+
+  },
+  computed:{
+
+  },
+  methods :{
+    showCurrentSlide(event){
+      this.url = this.slides[event.currentSlide].img
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./index.scss";
+</style>
+
+<style scoped>
+  .agile__slide {
+    display: block;
+    height: 678px;
+    -webkit-box-flex: 1;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+    -ms-flex-negative: 0;
+    flex-shrink: 0;
+  }
+</style>
---
+++ src/components/TestimonialSlider/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="testimonial-slider">
+    <agile :initialSlide="1" :autoplay="autoplay" :navButtons="false" :dots="true">
+      <div v-for="(slide, index) in slides" :key="index" class="slide-wrapper">
+        <div class="left-content">
+          <div class="heading-2 mb-4">"{{ slide.title }}" </div>
+          <div v-if="slide.text" class="heading-2 font-light" :style="textStyle">
+            {{ slide.text }}
+          </div>
+          <div v-if="slide.footer" class="footer text-sm text-gray-500 mt-4" :style="footerStyle">
+            {{ slide.footer }}
+          </div>
+        </div>
+        <div class="right-content">
+          <img :src="slide.urlImg" alt="" />
+        </div>
+      </div>
+    </agile>
+  </div>
+</template>
+
+<script>
+import { VueAgile } from 'vue-agile';
+
+export default {
+  name: 'TestimonialSlider',
+  components: {
+    'agile': VueAgile,
+  },
+  props: {
+    slides: {
+      type: Array,
+      required: false,
+    },
+    autoplay: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    textStyle: {
+      required: false,
+      type: Object,
+      default: () => {
+        return {
+          'font-size': '16px',
+        };
+      },
+    },
+    footerStyle: {
+      required: false,
+      type: Object,
+      default: () => {
+        return {
+          'font-size': '16px',
+        };
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+@import "./index.scss";
+</style>
