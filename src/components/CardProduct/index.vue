6c68139b613518f44fe7be9e62370e15f58e44ce
Windahr <wharyani@juraganmaterial.id>
2023-09-14 01:14:33 +0700

    feature: add service assets

---
+++ src/components/CardProduct/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="item">
+    <img class="img-product" :src="product.img" alt="" />
+    <div class="content">
+      <div class="item-title mb-2 text-sm"> {{ product.label }}</div>
+      <div class="price font-bold">{{ product.price }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: {
+    product: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./index.scss";
+</style>
